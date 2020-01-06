329f810e387aa43fead3689a594ea88859cd668b
Tich <tkames@hotmail.com>
2020-01-06 13:09:38 +0200

    Added integrations tests

---
+++ src/test/java/hod/springframework/spring5webapp/repositories/recipe/UnitOfMeasureRepositoryTestIT.java
@@ -0,0 +1,28 @@
+package hod.springframework.spring5webapp.repositories.recipe;
+
+import hod.springframework.spring5webapp.model.recipe.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+class UnitOfMeasureRepositoryTestIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+}
