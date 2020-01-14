68264f94adc07e15ca014bcd1a0f8968f903e566
Tich <tkames@hotmail.com>
2020-01-14 14:26:27 +0200

    Added controller advice

--- src/main/java/hod/springframework/spring5webapp/controllers/recipe/RecipeController.java
+++ src/main/java/hod/springframework/spring5webapp/controllers/recipe/RecipeController.java
@@ -1,12 +1,15 @@
 package hod.springframework.spring5webapp.controllers.recipe;
 
 import hod.springframework.spring5webapp.commands.recipe.RecipeCommand;
+import hod.springframework.spring5webapp.exceptions.NotFoundException;
 import hod.springframework.spring5webapp.model.recipe.Recipe;
 import hod.springframework.spring5webapp.services.recipe.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Set;
 
@@ -63,5 +66,20 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception){
+
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
     }
 }
