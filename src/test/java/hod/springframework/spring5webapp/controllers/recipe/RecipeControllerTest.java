a899dc329ffc04609557b6544ca5274bac05192e
Tich <tkames@hotmail.com>
2020-01-15 13:52:59 +0200

    Added form error messages

--- src/test/java/hod/springframework/spring5webapp/controllers/recipe/RecipeControllerTest.java
+++ src/test/java/hod/springframework/spring5webapp/controllers/recipe/RecipeControllerTest.java
@@ -79,6 +79,7 @@ public class RecipeControllerTest {
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
                 .param("description", "some string")
+                .param("directions", "directions")
         )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
