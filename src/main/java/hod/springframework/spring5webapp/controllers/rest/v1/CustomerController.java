f1691427c65a58a9ea7da4775b73607e49ff9433
Tich <tkames@hotmail.com>
2020-01-30 10:46:54 +0200

    Added swagger dependency

--- src/main/java/hod/springframework/spring5webapp/controllers/rest/v1/CustomerController.java
+++ src/main/java/hod/springframework/spring5webapp/controllers/rest/v1/CustomerController.java
@@ -3,11 +3,14 @@ package hod.springframework.spring5webapp.controllers.rest.v1;
 import hod.springframework.spring5webapp.api.v1.model.CustomerDTO;
 import hod.springframework.spring5webapp.api.v1.model.CustomerListDTO;
 import hod.springframework.spring5webapp.services.rest.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@Api(description = "This is my Customer Controller")
 @Controller
 @RequestMapping(CustomerController.BASE_URL)
 public class CustomerController {
@@ -20,6 +23,7 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @ApiOperation(value = "This will get a list of customers.", notes = "These are some notes about the API.")
     @GetMapping
     public ResponseEntity<CustomerListDTO> getListofCustomers(){
 
